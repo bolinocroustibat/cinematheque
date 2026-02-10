63294dd918f4ff70196b51bfb0f29d41eac91dce
Adrien Carpentier <me@adriencarpentier.com>
2026-02-10 23:30:37 +0100

    clean: lint backend code

--- backend/cinematheque_app/api/__init__.py
+++ backend/cinematheque_app/api/__init__.py
@@ -1,9 +1,9 @@
 from ninja import Router
 
-from cinematheque_app.api.movies import router as movies_router
-from cinematheque_app.api.series import router as series_router
 from cinematheque_app.api.books import router as books_router
+from cinematheque_app.api.movies import router as movies_router
 from cinematheque_app.api.palettes import router as palettes_router
+from cinematheque_app.api.series import router as series_router
 
 router = Router()
 router.add_router("/movies", movies_router, tags=["movies"])
